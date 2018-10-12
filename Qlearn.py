36fd670d3e4ccd97a0df2e20430876680d293c7d
Jeremy Dao <jeremydao913@gmail.com>
2018-10-12 12:05:08 -0700

    Completed simple Qlearn for single agent

--- Qlearn.py
+++ Qlearn.py
@@ -2,7 +2,9 @@ import random
 from random import randint
 import numpy as np
 import math
-import grid
+from grid import * 
+import time
+# import matplotlib.pyplot as plt
 
 class Qlearn:
 
@@ -10,20 +12,18 @@ class Qlearn:
         self.env = env
         self.dim_x = self.env.dim_x
         self.dim_y = self.env.dim_y
-        self.eps = .1       #Epsilon to act epsilon greedy by
-        #Intialize Q table with zeros; Qtable[action][x][y]
-        #Q value for state s where agent at (xa, ya) and target at (xt, yt) and action a:
-        #Qtable[a][yt*dim_y + ya][xt*dim_x + xa]
-        # self.Qtable = [[[0 for i in range(self.env.dim_x^2)] for i in range(self.env.dim_y^2)]
-                        # for i in range(self.env.params.action_dim)]
-        self.Qtable = np.zeros((self.dim_x^2, self.dim_y^2, self.env.params.action_dim))
+        self.eps = 0        #Epsilon to act epsilon greedy by
+        self.alpha = .1     #Learning rate
+        self.gamma = .8     #Discount factor
+        # Intialize Q table with zeros; Qtable[action][x][y]
         # Q value for state s where agent at (xa, ya) and target at (xt, yt) and action a:
         # Qtable[xt*dim_x + xa, yt*dim_y + ya, a]
         # Action 0, 1, 2, 3, correspond to up, right, down, left respectively
+        self.Qtable = np.zeros((self.dim_x**2, self.dim_y**2, self.env.params.action_dim))
 
     #Compute epsilon greedy action according to current Q values
     def get_action(self, rover_pos, target_pos):
-        if random.random > self.eps:        # Act greedily
+        if random.random() > self.eps:        # Act greedily
             actionVs = self.Qtable[target_pos[0]*self.dim_x + rover_pos[0],
                                 target_pos[1]*self.dim_y + rover_pos[1], :]
             direct = np.argmax(actionVs)
@@ -45,9 +45,48 @@ class Qlearn:
             action = [1, 0]
         elif direct == 2:
             action = [0, -1]
-        return action
+        return direct, action
+    
+    def train(self, niter):
+        mem_buff = np.zeros(100)
+        mem_iter = 0
+        filename = "Qtest.npy"
+        for i in range(niter):
+            self.env.reset()
+            done = False
+            while not done:
+                rov_old = self.env.rover_pos
+                tar_old = self.env.target_pos
+                direct, action = self.get_action(self.env.rover_pos, self.env.target_pos)
+                done = self.env.step(action)
+                reward = self.env.reward()
+                rov_new = self.env.get_rover_pos()
+                tar_new = self.env.get_target_pos()
+                Qxold = tar_old[0] * self.dim_x + rov_old[0]
+                Qyold = tar_old[1] * self.dim_y + rov_old[1]
+                Qxnew = tar_new[0] * self.dim_x + rov_new[0]
+                Qynew = tar_new[1] * self.dim_y + rov_new[1]
+                nextQmax = np.max(self.Qtable[Qxnew, Qynew, :])
+                Qold = self.Qtable[Qxold, Qyold, direct]
+                self.Qtable[Qxold, Qyold, direct] = (1 - self.alpha) * Qold + self.alpha \
+                    * (reward + self.gamma * nextQmax)
+                
+            mem_buff[mem_iter] = self.env.timestep
+            mem_iter += 1
+            if i % 100 == 0:
+                s = 'Iter: ' + str(i) + '\tMean: ' + str(np.mean(mem_buff)) + '\t Var: ' + str(np.var(mem_buff))
+                print(s)
+                mem_buff = np.zeros(100)
+                mem_iter = 0
+            if i % 1000 == 0:
+                np.save(filename, self.Qtable)
+                
 
-
+if __name__ == '__main__':
+    args = Parameters()
+    env = Task_Rovers(args)
+    learn = Qlearn(env)
+    learn.train(50000)
 
 
 
--- grid.py
+++ grid.py
@@ -3,6 +3,7 @@ from random import randint
 from operator import add
 import numpy as np
 import math
+import time
 
 class Parameters:
     def __init__(self):
@@ -36,22 +37,23 @@ class Task_Rovers:
         # Initialize rover position container
         self.rover_pos = [0, 0]     # Track rover's position
 
-        #Rover path trace (viz)
-        self.rover_path = [(self.rover_pos[0], self.rover_pos[1])]
-        # self.action_seq = [0.0 for _ in range(self.params.action_dim)]
-
-    def reset_rover(self, do_rand = True):
-        if do_rand:
-            self.rover_pos = [randint(1, self.dim_x), randint(1, self.dim_y)]
-        else:
-            self.rover_pos = [1, 1]
 
     def reset(self):
-        self.reset_rover()
-        self.rover_path = [(self.rover_pos[0], self.rover_pos[1])]
+        # self.rover_pos = [0, 0]
+        new_pos = [9, 3]
+        while new_pos == [9, 3]:
+            new_pos[0] = randint(0, self.dim_x - 1)
+            new_pos[1] = randint(0, self.dim_y - 1)
+        self.rover_pos = new_pos
         # Reset Target
-        self.target_pos = [2, 10]
+        self.target_pos = [9, 3]
         self.timestep = 0
+
+    def get_target_pos(self):
+        return self.target_pos
+    
+    def get_rover_pos(self):
+        return self.rover_pos
 
     def rand_action(self, pos):
         # Moves [0, 1, 2, 3] corresponds to up, right, down, left respectively
@@ -65,14 +67,20 @@ class Task_Rovers:
         if pos[1] == self.dim_y - 1:
             moves.remove(0)
         direct = random.choice(moves)
-        action = [-1, 0]
+        move = [-1, 0]
         if direct == 0:
-            action = [0, 1]
+            move = [0, 1]
         elif direct == 1:
-            action = [1, 0]
+            move = [1, 0]
         elif direct == 2:
-            action = [0, -1]
-        return action
+            move = [0, -1]
+        return move
+
+    def check_goal(self):
+        if self.rover_pos == self.target_pos:
+            return True
+        else:
+            return False
 
     def step(self, action):
         self.timestep += 1
@@ -88,36 +96,21 @@ class Task_Rovers:
         if self.rover_pos[1] >= self.dim_y:
             self.rover_pos[1] = self.dim_y - 1
         
+        if self.check_goal():
+            return True
+
         # Randomly move target
-        # Moves [1,2,3,4] corresponds to up, right, down, left respectively
-        # moves = [1, 2, 3, 4]
-        # if self.target_pos[0] == 0:
-        #     moves.remove(4)
-        # if self.target_pos[0] == self.dim_x - 1:
-        #     moves.remove(2)
-        # if self.target_pos[1] == 0:
-        #     moves.remove(3)
-        # if self.target_pos[1] == self.dim_y - 1:
-        #     moves.remove(1)
-        
-        # target_action = random.choice(moves)
-        # if target_action == 1:
-        #     self.target_pos[1] += 1
-        # elif target_action == 2:
-        #     self.target_pos[0] += 1
-        # elif target_action == 3:
-        #     self.target_pos[1] -= 1
-        # else:
-        #     self.target_pos[0] -= 1
         targ_act = self.rand_action(self.target_pos)
-        self.target_pos = self.target_pos + targ_act
-        print(self.target_pos)
-        print(targ_act)
+        self.target_pos[0] += targ_act[0]
+        self.target_pos[1] += targ_act[1]
+
+        return self.check_goal()
 
     def reward(self):
-        reward = -1*self.timestep
+        # reward = -1*self.timestep
+        reward = -1
         if self.rover_pos == self.target_pos:
-            reward += 20
+            reward = 20
         return reward
 
     def visualize(self):
@@ -151,14 +144,14 @@ class Task_Rovers:
         # Draw in rover path
         for rover_id in range(self.params.num_rover):
             for time in range(self.params.num_timestep):
-                x = int(self.rover_path[rover_id][time][0]);
+                x = int(self.rover_path[rover_id][time][0])
                 y = int(self.rover_path[rover_id][time][1])
                 # print x,y
                 grid[x][y] = drone_symbol_bank[rover_id]
 
         # Draw in food
         for loc, status in zip(self.poi_pos, self.poi_status):
-            x = int(loc[0]);
+            x = int(loc[0])
             y = int(loc[1])
             marker = 'I' if status else 'A'
             grid[x][y] = marker
@@ -176,8 +169,8 @@ if __name__ == '__main__':
         env.visualize()
         updown = random.choice([0, 1])
         leftright = random.choice([-1, 1])
-        action = [updown, leftright]
-        env.step(action)
+        act = [updown, leftright]
+        env.step(act)
 
 
 
